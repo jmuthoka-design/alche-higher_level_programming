3b88cd3009fdd4c69ae436386b9dc147ee3a8424
jmuthoka-design <j.muthoka@alustudent.com>
2026-08-23 06:31:41 -0700

    Task4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-new_in_list = __import__('4-new_in_list').new_in_list
+def new_in_list(my_list, idx, element):
+    """Replace an element in a copy of a list at a specific position
 
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
+    Args:
+        my_list: the original list (never modified)
+        idx: the index of the element to replace in the copy
+        element: the new value to place at idx
 
-print(new_list)
-print(my_list)
+    Returns:
+        A new list with the element replaced if idx is valid,
+        otherwise a copy of the original list
+    """
+    new_list = my_list[:]
+    if idx < 0:
+        return new_list
+    if idx >= len(my_list):
+        return new_list
+    new_list[idx] = element
+    return new_list
