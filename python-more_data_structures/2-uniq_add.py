eb233fd63cede7f795436c9ee75fdf1ba9a982da
jmuthoka-design <j.muthoka@alustudent.com>
2026-08-23 09:58:44 -0700

    Task2

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    """Add all unique integers in a list (only once for each integer)
+
+    Args:
+        my_list: list of integers
+
+    Returns:
+        The sum of each distinct integer in my_list, counted once
+    """
+    return sum(set(my_list))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+               
