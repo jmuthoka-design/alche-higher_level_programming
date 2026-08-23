7d58bcc23e1553beeefd7d6a4643db6daf405d27
jmuthoka-design <j.muthoka@alustudent.com>
2026-08-23 10:17:26 -0700

    Task5

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def number_keys(a_dictionary):
+    """Return the number of keys in a dictionary
+
+    Args:
+        a_dictionary: the dictionary to inspect
+
+    Returns:
+        The number of keys in a_dictionary
+    """
+    return len(a_dictionary)
