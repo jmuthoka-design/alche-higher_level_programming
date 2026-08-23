21ecc3e465bb92dbca3b9728f3f55df6510026e7
jmuthoka-design <j.muthoka@alustudent.com>
2026-08-23 09:37:28 -0700

    Task1

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    """Compute the square value of all integers of a matrix
+search_replace = __import__('1-search_replace').search_replace
 
-    Args:
-        matrix: a 2 dimensional list of integers
-
-    Returns:
-        A new matrix of the same size, with each value squared.
-        The original matrix is not modified.
-    """
-    return [[value ** 2 for value in row] for row in matrix]
+my_list = [1, 2, 3]
+new_list = search_replace(my_list, 1, 0)
+print(new_list)
+print(my_list)
