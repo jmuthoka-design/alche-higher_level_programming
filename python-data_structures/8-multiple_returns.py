53f39c85cad8d8e5aeeb2dcca0bec842d30cebfa
jmuthoka-design <j.muthoka@alustudent.com>
2026-08-23 08:35:20 -0700

    Task8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
-multiple_returns = __import__('8-multiple_returns').multiple_returns
+def multiple_returns(sentence):
+    """Return the length of a string and its first character
 
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+    Args:
+        sentence: the string to inspect
+
+    Returns:
+        A tuple (length, first_character). first_character is None
+        if the sentence is empty.
+    """
+    if len(sentence) == 0:
+        return (0, None)
+    return (len(sentence), sentence[0])
