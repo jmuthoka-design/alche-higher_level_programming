4f55dd3780a426e7a37493d2e0221fd49132b155
jmuthoka-design <j.muthoka@alustudent.com>
2026-08-23 10:10:52 -0700

    Task3

---
+++ python-more_data_structures/3-common_elements.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    """Return the set of common elements between two sets
+
+    Args:
+        set_1: the first set
+        set_2: the second set
+
+    Returns:
+        A new set containing the elements present in both sets
+    """
+    return set_1 & set_2
