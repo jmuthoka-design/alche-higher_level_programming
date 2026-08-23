75e6f797b62cf02212d966eeec1959ea471bf367
jmuthoka-design <j.muthoka@alustudent.com>
2026-08-23 10:04:34 -0700

    Task2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-mod = __import__('0-square_matrix_simple')
-square_matrix_simple = mod.square_matrix_simple
+def uniq_add(my_list=[]):
+    """Add all unique integers in a list (only once for each integer)
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+    Args:
+        my_list: list of integers
 
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)            
+    Returns:
+        The sum of each distinct integer in my_list, counted once
+    """
+    return sum(set(my_list))
