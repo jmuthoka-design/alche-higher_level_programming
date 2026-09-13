7f6727a0c50008a8eb719288b85b74fd594e4566
jmuthoka-design <jmuthoka@alustudent.com>
2026-09-13 22:05:21 +0400

    task9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class that inherits from BaseGeometry."""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle with validated private dimensions."""
+
+    def __init__(self, width, height):
+        """Initialize a rectangle with a width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return the rectangle description."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+    
