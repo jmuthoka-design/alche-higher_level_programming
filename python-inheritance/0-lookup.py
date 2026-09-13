5150ec7406a7812eb7bc937725af7b221c9a4526
jmuthoka-design <jmuthoka@alustudent.com>
2026-09-13 21:07:14 +0400

    task0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Provides a function to list an object's attributes and methods."""
+
 
 def lookup(obj):
+    """Return a list of an object's attributes and methods."""
     return dir(obj)
