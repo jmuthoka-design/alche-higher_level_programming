facda868d6e933af48aaecb37ce99639362ae59e
jmuthoka-design <jmuthoka@alustudent.com>
2026-09-13 21:46:39 +0400

    task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines the BaseGeometry class."""
+
+
+class BaseGeometry:
+    """Represents a base geometry class."""
+
+    def area(self):
+        """Raise an exception because area is not implemented."""
+        raise Exception("area() is not implemented")
+    
