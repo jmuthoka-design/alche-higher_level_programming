b3a5a4c87f36f6891fc04f49b60106e55204cce6
jmuthoka-design <jmuthoka@alustudent.com>
2026-09-24 12:47:19 +0400

    task1

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Displays the response body only when the HTTP status code is 200.
-curl -s -f "$1"
+# Sends a GET request to the given URL and displays the body only if the status code is 200
+curl -s -w '\n%{http_code}' "$1" | awk 'NR==1{b=$0;next}{c=$0}END{if(c==200)print b}'
