bb124919cc7936aaa4988e84d2f374341fb05dee
jmuthoka-design <jmuthoka@alustudent.com>
2026-09-24 11:47:26 +0400

    task0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
--- List privileges of user_0d_1
+-- List all privileges of user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
--- List privileges of user_0d_2
+-- List all privileges of user_0d_2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
