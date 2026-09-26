bbc4b1d67df87993f9a9002d7161c19d5ab4e0d9
jmuthoka-design <jmuthoka@alustudent.com>
2026-09-26 14:30:57 +0400

    task0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""This module fetches and displays the status of the ALU intranet server."""
+"""Fetches a URL and displays information about its response body."""
 import urllib.request
 
 
-with urllib.request.urlopen("https://alu-intranet.hbtn.io/status") as response:
-    body = response.read()
-    print("Body response:")
-    print("\t- type:", type(body))
-    print("\t- content:", body)
-    print("\t- utf8 content:", body.decode("utf-8"))
+def fetch_status(url):
+    """Fetches the given URL and displays its response body information."""
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode("utf-8"))
+
+
+if __name__ == "__main__":
+    fetch_status("https://alu-intranet.hbtn.io/status")
